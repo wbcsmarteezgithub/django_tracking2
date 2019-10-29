2d4b7edf976c5bd00fcbf2564a214cecf8c8f472
wbc <abdelaaziz.sisti@smarteez.eu>
2019-10-29 09:19:50 +0100

    change filter by user and ip to search

--- tracking/admin.py
+++ tracking/admin.py
@@ -3,20 +3,24 @@ from django.contrib import admin
 from tracking.models import Visitor, Pageview
 from tracking.settings import TRACK_PAGEVIEWS
 
+
 class VisitorAdmin(admin.ModelAdmin):
     date_hierarchy = 'start_time'
 
     list_display = ('session_key', 'user', 'start_time', 'session_over',
-        'pretty_time_on_site', 'ip_address', 'user_agent')
-    list_filter = ('user', 'ip_address')
+                    'pretty_time_on_site', 'ip_address', 'user_agent')
+    # list_filter = ('user', 'ip_address')
+    search_fields = ('user__username', 'ip_address',)
 
     def session_over(self, obj):
         return obj.session_ended() or obj.session_expired()
+
     session_over.boolean = True
 
     def pretty_time_on_site(self, obj):
         if obj.time_on_site is not None:
             return timedelta(seconds=obj.time_on_site)
+
     pretty_time_on_site.short_description = 'Time on site'
 
 
